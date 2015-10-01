60a0f80bb9bacddc0a8c15b84c5140e01ffbb474
phuoc <maithanhphuoc1991@gmail.com>
2015-10-01 10:51:28 +0700

    test

--- BaiTapiOS/ViewController.swift
+++ BaiTapiOS/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//elements
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,18 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+//actions
+    @IBAction func setLabel(sender: UIButton) {
+    if nameTextField.text == ""
+    {let alert = UIAlertController(title: "Caution!",
+            message: "Please insert above textfield!", preferredStyle: .Alert)
+            let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+            alert.addAction(action)
+        presentViewController(alert, animated: true, completion: nil)}
+    else
+    {nameLabel.text = nameTextField.text
+        nameTextField.text = nil}}
+    
 
 }
 
